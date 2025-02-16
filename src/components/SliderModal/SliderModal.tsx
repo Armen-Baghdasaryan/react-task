3f67c5567b81cca166eec12c5272fe67ca37793a
Armen Baghdasaryan <armen.baghdasaryan91@gmail.com>
2025-02-16 12:50:55 +0400

    modify sliding functionality

--- src/components/SliderModal/SliderModal.tsx
+++ src/components/SliderModal/SliderModal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import './slider-modal.scss';
 
 import closeIcon from "/src/assets/icons/arrow-close.svg";
@@ -11,32 +11,26 @@ interface PageProps {
 }
 
 const SliderModal = ({ images, open, close }: PageProps) => {
-    const [src, setSrc] = useState<string | null>(null);
     const [imgIndex, setImgIndex] = useState<number>(0);
     const [isFading, setIsFading] = useState<boolean>(false);
     const [startTouch, setStartTouch] = useState<number>(0);
     const [isSwiping, setIsSwiping] = useState<boolean>(false);
 
-    useEffect(() => {
-        setSrc(images[0] || null);
-        setImgIndex(0)
-    }, [images]);
-
-    useEffect(() => {
+    const changeImages = (index: number) => {
         setIsFading(true)
 
         setTimeout(() => {
-            setSrc(images[imgIndex])
+            setImgIndex(index)
             setIsFading(false);
         }, 200)
-    }, [imgIndex])
+    }
 
     const onSlideImgClick = (index: number) => {
-        setImgIndex(index);
+        changeImages(index)
     };
 
     const handleSlide = (action: string) => {
-        action === 'next' ? setImgIndex((prev: number) => prev + 1) : setImgIndex((prev: number) => prev - 1)
+        action === 'next' ? changeImages(imgIndex + 1) : changeImages(imgIndex - 1)
     }
 
     const handleTouchStart = (e: React.TouchEvent) => {
@@ -52,9 +46,9 @@ const SliderModal = ({ images, open, close }: PageProps) => {
             if (Math.abs(swipeDistance) > 50 && !isSwiping) {
                 setIsSwiping(true);
                 if (swipeDistance > 0 && imgIndex > 0) {
-                    setImgIndex((prev) => prev - 1);
+                    changeImages(imgIndex - 1)
                 } else if (swipeDistance < 0 && imgIndex < images.length - 1) {
-                    setImgIndex((prev) => prev + 1);
+                    changeImages(imgIndex + 1)
                 }
             }
         }
@@ -72,11 +66,9 @@ const SliderModal = ({ images, open, close }: PageProps) => {
                     <p className="slider_count">{imgIndex + 1}/{images.length}</p>
                     <div className="slider_main">
                         <img width={32} height={32} src={arrowLeft} alt="arrow" className={`slider_arrow ${imgIndex === 0 ? 'slider_arrow-disabled' : ''}`} onClick={() => handleSlide('prev')} />
-                        {src && (
-                            <div onTouchStart={handleTouchStart} onTouchMove={handleTouchMove} onTouchEnd={handleTouchEnd} className="main_img_container">
-                                <img className={`slider_main_img ${isFading ? "fade-out" : "fade-in"}`} src={src} alt="main-img" />
-                            </div>
-                        )}
+                        <div onTouchStart={handleTouchStart} onTouchMove={handleTouchMove} onTouchEnd={handleTouchEnd} className="main_img_container">
+                            <img className={`slider_main_img ${isFading ? "fade-out" : "fade-in"}`} src={images[imgIndex || 0]} alt="main-img" />
+                        </div>
                         <img width={32} height={32} src={arrowLeft} alt="arrow" className={`slider_arrow slider_arrow-next ${imgIndex === images.length - 1 ? 'slider_arrow-disabled' : ''}`} onClick={() => handleSlide('next')} />
                     </div>
                     <div className="slider_images">
